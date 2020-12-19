0a333171de12666b431880f4d0ac94076c1910da
pjvilloud <pjvilloud@protonmail.com>
2020-12-19 16:01:27 +0100

    Completed day 16 part2!

--- day16.py
+++ day16.py
@@ -1,4 +1,69 @@
-import re
+"""--- Day 16: Ticket Translation ---
+As you're walking to yet another connecting flight, you realize that one of the legs of your re-routed trip coming up is on a high-speed train. However, the train ticket you were given is in a language you don't understand. You should probably figure out what it says before you get to the train station after the next flight.
+
+Unfortunately, you can't actually read the words on the ticket. You can, however, read the numbers, and so you figure out the fields these tickets must have and the valid ranges for values in those fields.
+
+You collect the rules for ticket fields, the numbers on your ticket, and the numbers on other nearby tickets for the same train service (via the airport security cameras) together into a single document you can reference (your puzzle input).
+
+The rules for ticket fields specify a list of fields that exist somewhere on the ticket and the valid ranges of values for each field. For example, a rule like class: 1-3 or 5-7 means that one of the fields in every ticket is named class and can be any value in the ranges 1-3 or 5-7 (inclusive, such that 3 and 5 are both valid in this field, but 4 is not).
+
+Each ticket is represented by a single line of comma-separated values. The values are the numbers on the ticket in the order they appear; every ticket has the same format. For example, consider this ticket:
+
+.--------------------------------------------------------.
+| ????: 101    ?????: 102   ??????????: 103     ???: 104 |
+|                                                        |
+| ??: 301  ??: 302             ???????: 303      ??????? |
+| ??: 401  ??: 402           ???? ????: 403    ????????? |
+'--------------------------------------------------------'
+Here, ? represents text in a language you don't understand. This ticket might be represented as 101,102,103,104,301,302,303,401,402,403; of course, the actual train tickets you're looking at are much more complicated. In any case, you've extracted just the numbers in such a way that the first number is always the same specific field, the second number is always a different specific field, and so on - you just don't know what each position actually means!
+
+Start by determining which tickets are completely invalid; these are tickets that contain values which aren't valid for any field. Ignore your ticket for now.
+
+For example, suppose you have the following notes:
+
+class: 1-3 or 5-7
+row: 6-11 or 33-44
+seat: 13-40 or 45-50
+
+your ticket:
+7,1,14
+
+nearby tickets:
+7,3,47
+40,4,50
+55,2,20
+38,6,12
+It doesn't matter which position corresponds to which field; you can identify invalid nearby tickets by considering only whether tickets contain values that are not valid for any field. In this example, the values on the first nearby ticket are all valid for at least one field. This is not true of the other three nearby tickets: the values 4, 55, and 12 are are not valid for any field. Adding together all of the invalid values produces your ticket scanning error rate: 4 + 55 + 12 = 71.
+
+Consider the validity of the nearby tickets you scanned. What is your ticket scanning error rate?
+
+Your puzzle answer was 26009.
+
+--- Part Two ---
+Now that you've identified which tickets contain invalid values, discard those tickets entirely. Use the remaining valid tickets to determine which field is which.
+
+Using the valid ranges for each field, determine what order the fields appear on the tickets. The order is consistent between all tickets: if seat is the third field, it is the third field on every ticket, including your ticket.
+
+For example, suppose you have the following notes:
+
+class: 0-1 or 4-19
+row: 0-5 or 8-19
+seat: 0-13 or 16-19
+
+your ticket:
+11,12,13
+
+nearby tickets:
+3,9,18
+15,1,5
+5,14,9
+Based on the nearby tickets in the above example, the first position must be row, the second position must be class, and the third position must be seat; you can conclude that in your ticket, class is 12, row is 11, and seat is 13.
+
+Once you work out which field is which, look for the six fields on your ticket that start with the word departure. What do you get if you multiply those six values together?
+
+Your puzzle answer was 589685618167."""
+
+import copy
 
 input = """departure location: 49-920 or 932-950
 departure station: 28-106 or 130-969
@@ -264,20 +329,6 @@ nearby tickets:
 196,809,174,498,674,64,291,852,803,550,202,630,63,499,460,938,767,133,13,101
 896,791,715,95,512,199,854,490,672,50,990,943,556,630,261,57,520,890,266,187"""
 
-input = """class: 1-3 or 5-7
-row: 6-11 or 33-44
-seat: 13-40 or 45-50
-
-your ticket:
-7,1,14
-
-nearby tickets:
-7,3,47
-40,4,50
-55,2,20
-38,6,12"""
-# B=list(set(a)&set(b))
-# { "class": [[0,1]], "row": [[0]]
 split_input = input.split("\n\n")
 
 rules = dict()
@@ -322,3 +373,56 @@ for t in nearby_tickets:
         valid_tickets.append(t)
 
 print "The ticket scanning error rate is", sum_invalid_values
+
+# Part 2
+invalid_rules_for_valid_tickets = dict()
+for rul in rules:
+    invalid_rules_for_valid_tickets[rul] = set([])
+
+for vt in valid_tickets:
+    split_ticket = vt.split(",")
+    for j in range(len(split_ticket)):
+        for ru in rules:
+            if not value_is_valid(int(split_ticket[j]), rules[ru]):
+                invalid_rules_for_valid_tickets[ru].add(j)
+
+print invalid_rules_for_valid_tickets
+
+rules_for_valid_tickets = dict()
+
+for rul in invalid_rules_for_valid_tickets.keys():
+    rules_for_valid_tickets[rul] = set([])
+    for index in range(len(your_ticket)):
+        if index not in invalid_rules_for_valid_tickets[rul]:
+            rules_for_valid_tickets[rul].add(index)
+
+print rules_for_valid_tickets
+
+same_twice = False
+while not same_twice:
+    copy_of_rules_for_valid_tickets = copy.deepcopy(rules_for_valid_tickets)
+    only_fields = list(filter(lambda rk: len(rules_for_valid_tickets[rk]) == 1, rules_for_valid_tickets.keys()))
+    found_fields = list(map(lambda ff: (ff, list(rules_for_valid_tickets[ff])[0]), only_fields))
+    for found_field in found_fields:
+        for rfvt in rules_for_valid_tickets.keys():
+            if rfvt != found_field[0] and found_field[1] in rules_for_valid_tickets[rfvt]:
+                rules_for_valid_tickets[rfvt].remove(found_field[1])
+    print found_fields
+    same_twice = copy_of_rules_for_valid_tickets == rules_for_valid_tickets
+
+
+def is_arrangement_ok(fa):
+    # We check each ticket against the field arrangement
+    for valid_ticket in valid_tickets:
+        split_ticket = valid_ticket.split(",")
+        for j in range(len(split_ticket)):
+            if not value_is_valid(int(split_ticket[j]), rules[fa[j]]):
+                return False
+    return True
+
+
+answer = 1
+for field_with_pos in rules_for_valid_tickets:
+    if field_with_pos.startswith("departure"):
+        answer *= int(your_ticket[int(list(rules_for_valid_tickets[field_with_pos])[0])])
+print "The answer is", answer
